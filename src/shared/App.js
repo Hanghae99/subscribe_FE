f30979db3383dc70b70689442939a6fdb467ccb5
Kopite93 <1000nulee@naver.com>
2022-04-24 14:24:58 +0900

    initial

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "../page/Login";
+import SignUp from "../page/SignUp";
 
 function App() {
-  return <div className="App">9duck 9duck</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
