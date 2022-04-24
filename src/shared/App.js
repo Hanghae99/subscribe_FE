f509561ec62f770280564cf08190712b9566e5cf
Kopite93 <1000nulee@naver.com>
2022-04-24 18:23:35 +0900

    initial

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "../page/Login";
 import SignUp from "../page/SignUp";
+import MyPage from "./../page/MyPage";
+import Main from "../page/Main";
+import Detail from "./../page/Detail";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/detail" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
